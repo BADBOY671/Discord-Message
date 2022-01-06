ce7087498f763ff4824f790bc9e87cb5c37fee9e
BADBOY671 <86197607+BADBOY671@users.noreply.github.com>
2022-01-06 21:43:05 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose')
+const Discord = require('discord.js')
+let msg = require('../models/message.js')
+const client = new Discord.Client()
+  async function Setup(client, url){
+    if(!client) return
+    if (!url) return 
+    return mongoose.connect(url, {
+      useNewUrlParser: true,
+    });
+  }
+Setup(process.env.token, process.env.database)
+async function onMessage({userID, guildID, message}){
+let data = await msg.findOne({userID: userID, guildID: guildID})
+if(!data || data == null){
+let s =  new msg({ userID: userID, guildID: guildID,count: 1 }).save()
+
+} else {
+data.count = parseInt(data.count) + parseInt(1)
+await data.save()
+
+}
+}
+async function AddMessage({userID, guildID, value}){
+let data = await msg.findOne({userID: userID, guildID: guildID})
+if(!data || data == null){
+new msg({ userID: userID, guildID: guildID,count: parseInt(value) }).save()
+}
+data.count += parseInt(value)
+await data.save().catch(e => console.log(e));
+}
+
+
+
+async function SubtractMessage({userID, guildID, message, count}){
+let data = await msg.findOne({userID: userID, guildID: guildID})
+if(!data || data == null){
+return;
+
+} else {
+data.count -= count
+await data.save().catch(e => console.log(e));
+}
+
+}
+async function Fetch({userID, guildID}){
+return new Promise(async (resolve, reject) => {
+let data = await msg.findOne({userID: userID, guildID: guildID})
+if(!data || data == null){
+return data = 0
+
+} else {
+resolve(data.count)
+}
+
+})
+}
+
+
+async function Leaderboard({guildID, UsersFetch}){
+return new Promise(async (resolve, reject) => {
+    let data = await msg.find({guildID: guildID}) 
+
+ let news = data.sort((a,b) => b.count-a.count)
+let array =[]
+for(let i=0; i < UsersFetch; i++){
+if(news[i] === undefined ) continue;
+array.push(news[i])
+}
+    let test = array.map((c,i) => `${++i} - <@!${c.userID}> - ${c.count}`)
+
+    let final = test.join("\n")
+resolve(final)
+    
+})
+}
+module.exports.Setup = Setup;
+module.exports.onMessage = onMessage;
+module.exports.AddMessage = AddMessage;
+module.exports.SubtractMessage = SubtractMessage;
+module.exports.Fetch = Fetch;
+module.exports.Leaderboard = Leaderboard;
